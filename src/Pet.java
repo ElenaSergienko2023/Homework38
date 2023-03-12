c7b201c66c54a546f18001689de024ad374ef63d
Elena Sergienko <marketing.seo.web2020@gmail.com>
2023-03-12 17:05:04 +0300

    Add enum, constructors, getters, setters

--- src/Pet.java
+++ src/Pet.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Pet {
 //  Создать класс Pet (домашнее животное). В классе должны быть:
 //  enum Kind для вида животного (DOG, CAT, OTHER);
 //  поля:
@@ -12,4 +12,59 @@ public class Main {
 //  Создать класс Main, в котором данные будут считываться с консоли и красиво выводиться на экран.
 //  Формат входных данных: количество строк с записями о домашних животных,
 //  атем сами записи в описанном формате.
+
+  final private static char SEP = ',';
+  private Kind kind;
+  private String name;
+  private String dateOfBirth;
+  private double weight;
+
+  public Pet(String name, String kind) {
+    this.name = name;
+    this.kind = kind;
+    this.dateOfBirth = null;
+    this.weight = null;
+  }
+
+  public Pet(String name, String kind, String dateOfBirth, double weight) {
+    this.name = name;
+    this.kind = kind;
+    this.dateOfBirth = dateOfBirth;
+    this.weight = weight;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getKind() {
+    return kind;
+  }
+
+  public String getDateOfBirth() {
+    return dateOfBirth;
+  }
+
+  public double getWeight() {
+    return weight;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setKind(String kind) {
+    this.kind = kind;
+  }
+
+  public void setDateOfBirth(String dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  public void setWeight(String weight) {
+    this.weight = weight;
+  }
+
+
+
 }
